600ffd24b442991e3df405149b566c1265cbe572
miketigos <tigos2012@yahoo.com>
2021-02-14 18:11:43 -0800

    second commit

--- GuessTheMovie.java
+++ GuessTheMovie.java
@@ -1,18 +1,18 @@
 import java.io.*;
-import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.Scanner;
 
 public class GuessTheMovie {
 
     // Data members for the game
-    private ArrayList<Character> guessedLetters = null;
-    private StringBuilder guessString;
-    private int GuessesRemaining;
+    private ArrayList<Character> guessedLetters;
+    private String movieName;
+    private int guessesRemaining;
+    String guessString;
+    boolean hasWon;
 
     /**
      * playGame will have the following functions in the program:
-     * -Select the movie to be guessed (random.randint)
      * -convert the movie name to all dashes (guessString)
      * -play the game loop:
      *    -do, check if user input is in string
@@ -21,19 +21,72 @@ public class GuessTheMovie {
      *           the character guess ot the guessedLetters list
      *         - while (!hasWon)
      */
+    public GuessTheMovie(String filename) throws IOException {
+        this.movieName = getRandomMovie(filename);
+        guessesRemaining = 5;
+        guessedLetters = new ArrayList<>();
+        guessString = movieName.replaceAll("[a-zA-Z]", "-");
+        hasWon = false;
+    }
 
-    public static void playGame() {
+    public void playGame() {
+        Scanner userInput = new Scanner(System.in);
 
+        do {
+            System.out.println("Guess a letter: ");
+            String guess = String.valueOf(userInput.next().charAt(0));
+
+
+
+            /**
+             * check if the movie name contains the guessed letter
+             * if so then we will loop through the characters in the movie name
+             * and use the stringbuilder.setcharat method to replace the "-" with the guessed letter
+             * when no more "-" remain we will end the loop
+             */
+            if (movieName.contains(guess)) {
+                char guessedLetter = guess.charAt(0);
+                StringBuilder sb = new StringBuilder(guessString);
+
+                if (guessedLetters.contains(guessedLetter)) {
+                    System.out.println("You already guessed that letter, try again...");
+                    continue;
+                } else {
+                    guessedLetters.add(guessedLetter);
+                }
+
+                for (int i = 0; i < sb.length(); i++) {
+                    if (movieName.charAt(i) == guessedLetter)
+                        sb.setCharAt(i, guessedLetter);
+                }
+                guessString = sb.toString();
+            } else {
+                guessesRemaining--;
+            }
+            hasWon = hasWon();
+            System.out.println(guessString);
+            System.out.println(guessesRemaining + " guesses remain");
+
+        } while (guessesRemaining > 0 && !hasWon);
+        userInput.close();
+
+        if (hasWon) {
+            System.out.println("You have won! Congratulations!");
+        } else {
+            System.out.println("You lost, name of the movie was " + movieName);
+        }
     }
 
     /**
      * this function will check if the string has no more
      * dashes left in it
-     * if no dahses remain we will return true
+     * if no dashes remain we will return true
      * if there are still dashes in the guessString we will return false
      */
     public boolean hasWon() {
-        return true;
+        if (guessString.equals(movieName))
+            return true;
+        return false;
     }
 
     /**
@@ -41,14 +94,14 @@ public class GuessTheMovie {
      * copies them into a dynamic array, then we will return a random
      * movie from the list using the math.random() function
      */
-    public String getRandomMovie(File movieList) throws IOException {
+    public static String getRandomMovie(String fileName) throws IOException {
         ArrayList<String> movies = new ArrayList<>();
         String line;
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader(movieList));
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
             while ((line = br.readLine()) != null) {
-                movies.add(line);
+                movies.add(line.trim());
             }
         } catch (IOException e) {
             System.out.println("file not found, check file path exists");
--- Main.java
+++ Main.java
@@ -1,14 +1,21 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        Scanner input = new Scanner(System.in);
         System.out.print("Welcome to the movie guessing game! \n" +
                 "You will have 10 guesses to find all the letters of \n" +
                 "the movie to be guess. Guess al the letter in time and you win \n" +
-                "run out of guesses you lose. Simple. Lets get started!\n");
+                "run out of guesses you lose. Simple. Lets get started!\n" +
+                "\n Please enter the name of the movie list file: ");
+        String filename = input.nextLine();
+        GuessTheMovie game = new GuessTheMovie(filename);
+        game.playGame();
+        input.close();
 
     }
 }
